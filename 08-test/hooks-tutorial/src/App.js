738c5db2a2ba1244d2b40c010ef752e2ef6130f8
hankh <hankh.dev@gmail.com>
2023-04-14 12:02:40 +0900

    test: add test code

---
+++ 08-test/hooks-tutorial/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Average from './Average';
+import Info from './Info';
+
+const App = () => {
+  // return <Average />;
+  return <Info />;
+};
+
+export default App;
